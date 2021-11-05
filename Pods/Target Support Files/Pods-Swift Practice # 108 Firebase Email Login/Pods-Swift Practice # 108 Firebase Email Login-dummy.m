4961e5b7700fbb394e6e6af7bcfff85d06d473bc
Dogpa <dogpa0130@gmail.com>
2021-11-05 22:22:29 +0800

    demo

---
+++ Pods/Target Support Files/Pods-Swift Practice # 108 Firebase Email Login/Pods-Swift Practice # 108 Firebase Email Login-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift_Practice___108_Firebase_Email_Login : NSObject
+@end
+@implementation PodsDummy_Pods_Swift_Practice___108_Firebase_Email_Login
+@end
